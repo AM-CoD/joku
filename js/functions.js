eb86187a564005e61a43691157d2b3b0c301e23c
amar mahdi <amar.mahdi99@icloud.com>
2024-02-06 13:40:40 +0200

    joku

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+let rand_num1 = 0
+let rand_num2 = 0 
+
+const getrandomintnumberinrange = (min,max) => {
+    return Math.floor(math.random()* max)+min;
+}
+const randomizenumber = () => {
+    rand_num1 = getrandomintnumberinrange(1,10)
+    rand_num2 = getrandomintnumberinrange(1.10)
+
+
+
+}
